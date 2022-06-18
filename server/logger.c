f0c07f52f235a614fa802f686f6f92e8fdb7d220
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2022-06-18 01:58:41 -0300

    Added time to logger

--- server/logger.c
+++ server/logger.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include "logger.h"
+
+#define DATE_LENGTH 21
+#define DATE_FORMAT "%FT%TZ"
 
 struct ip_and_port {
     char ip[INET6_ADDRSTRLEN + 1];
@@ -56,16 +60,20 @@ void logger(enum logType type, socks5_connection *conn) {
             return;
     }
 
-    // TODO agregar fecha en formato ISO-8601
-    // TODO agregar user que se conecta
+    char time_str[DATE_LENGTH];
+    time_t t = time(NULL);
+    struct tm tm;
+    localtime_r(&t, &tm);
+    strftime(time_str, DATE_LENGTH, DATE_FORMAT, &tm);
+
     switch (type) {
         case LOG_ACCESS:
             sockaddr_to_human(&source_ip_port, (struct sockaddr*) &conn->client_addr);
-            printf("%s\t%s\t%c\t%s\t%hu\t%s\t%hu\t%d\n", "fecha", user, type, source_ip_port.ip,
+            printf("%s\t%s\t%c\t%s\t%hu\t%s\t%hu\t%d\n", time_str, user, type, source_ip_port.ip,
                    htons(source_ip_port.port), destination, ntohs(conn->parser.request.request.port), conn->parser.request.response.status);
             break;
         case LOG_PASSWORD:
-            printf("%s\t%s\t%c\tPOP3\t%s\t%hu\t%s\t%s\n", "fecha", user, type, destination,
+            printf("%s\t%s\t%c\tPOP3\t%s\t%hu\t%s\t%s\n", time_str, user, type, destination,
                    ntohs(conn->parser.request.request.port), conn->pop3.info.user, conn->pop3.info.pass);
             break;
         default:
